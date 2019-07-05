a90898446dfea5879010256bbc087fb9d5dd0ed4
Bennett J. Bremer <bbremer@wisc.edu>
2019-07-04 22:51:35 -0500

    Deletes old files and checks for YCM

--- autoinstall.sh
+++ autoinstall.sh
@@ -14,6 +14,10 @@ files="vimrc vim"    # list of files/folders to symlink in homedir
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
+if [ -d $olddir ]
+then
+    rm -rf $olddir
+fi
 mkdir -p $olddir
 echo "...done"
 
@@ -34,6 +38,11 @@ git clone https://github.com/gmarik/Vundle.vim.git vim/bundle/Vundle.vim
 
 vim +PluginInstall +qall
 
-cd vim/bundle/YouCompleteMe
-
-python3 install.py --clang-completer
+ycmdir=vim/bundle/YouCompleteMe
+echo $ycmdir
+if [ -d $ycmdir ]
+then
+    echo "Installing youcompleteme"
+    cd $ycmdir
+    python3 install.py --clang-completer
+fi
